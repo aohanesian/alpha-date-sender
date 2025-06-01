36bf112187bda647e2a57215d148832a031a63c2
aohanesian <103946712+aohanesian@users.noreply.github.com>
2025-06-01 20:24:18 +0200

    20

--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -1,32 +1,28 @@
-import { Request as ExpressRequest, Response as ExpressResponse, NextFunction as ExpressNextFunction, Router as ExpressRouter, Application as ExpressApplication } from 'express';
+import { 
+  Request as ExpressRequest, 
+  Response as ExpressResponse, 
+  NextFunction as ExpressNextFunction, 
+  Router as ExpressRouter, 
+  Application as ExpressApplication,
+  RequestHandler as ExpressRequestHandler
+} from 'express';
 import { CustomJwtPayload } from './jwt';
 
+// Augment the Express namespace to add custom properties
 declare global {
   namespace Express {
     interface Request {
       user?: CustomJwtPayload;
-      body: any;
-      params: any;
-      headers: any;
-    }
-    interface Response {
-      json: (body: any) => Response;
-      status: (code: number) => Response;
-    }
-    interface NextFunction {
-      (err?: any): void;
-    }
-    interface Application {
-      use: (handler: any) => Application;
-      json: () => any;
-      static: (path: string) => any;
     }
   }
 }
 
-export type Request = ExpressRequest & Express.Request;
-export type Response = ExpressResponse & Express.Response;
-export type NextFunction = ExpressNextFunction & Express.NextFunction;
-export type Application = ExpressApplication & Express.Application;
-export const Router = ExpressRouter;
-export type RequestHandler = (req: Request, res: Response, next: NextFunction) => void; 
+// Export the Express types directly (no intersection needed)
+export type Request = ExpressRequest;
+export type Response = ExpressResponse;
+export type NextFunction = ExpressNextFunction;
+export type Application = ExpressApplication;
+export type RequestHandler = ExpressRequestHandler;
+
+// Export Router as a value, not a type
+export { Router as ExpressRouter } from 'express';
